f7e14933dc9e3e5b8c43b32c2f26503772534cf1
Greekkgod <pradhumaygaur@myyahoo.com>
2025-05-21 16:27:17 +0530

    its a basic ai mdoel

---
+++ main.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_anthropic import ChatAnthropic
+
+load_dotenv()
+
+llm = ChatAnthropic(model ="claude-3-5-sonnet-20241022")
+response = llm.invoke("what is the meaning of life?")
+print(response)
